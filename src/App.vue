523670e404a731f4a5717dcab7d36c047f15aa40
wwj202 <wangweijie_202@163.com>
2018-03-19 00:13:32 +0800

    add form of product

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,42 @@
 <template>
   <div id="app">
-    <router-view/>
+    <el-row>
+      <el-col :span="4">
+        <div style="background-color: #545c64; color: white; font-size: 30px; padding: 20px 20px">系统菜单</div>
+        <el-menu
+          default-active="/product"
+          class="el-menu-vertical-demo"
+          background-color="#545c64"
+          text-color="#fff"
+          active-text-color="#ffd04b"
+          :default-active="$route.path"
+          :router="true">
+          <el-menu-item index="/product">
+            <i class="el-icon-menu"></i>
+            <span slot="title">产品管理</span>
+          </el-menu-item>
+          <el-menu-item index="/in">
+            <i class="el-icon-download"></i>
+            <span slot="title">入库管理</span>
+          </el-menu-item>
+          <el-menu-item index="/out">
+            <i class="el-icon-upload2"></i>
+            <span slot="title">出库管理</span>
+          </el-menu-item>
+          <el-menu-item index="/subsidy">
+            <i class="el-icon-date"></i>
+            <span slot="title">报单补助管理</span>
+          </el-menu-item>
+          <!-- <el-menu-item index="/test">
+            <i class="el-icon-setting"></i>
+            <span slot="title">测试</span>
+          </el-menu-item> -->
+        </el-menu>
+      </el-col>
+      <el-col :span="20">
+        <router-view/>
+      </el-col>
+    </el-row>
   </div>
 </template>
 
@@ -15,8 +51,8 @@ export default {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  /* text-align: center; */
+  /* text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 60px; */
 }
 </style>
--- src/views/product/productList.vue
+++ src/views/product/productList.vue
@@ -1,7 +1,39 @@
 <template>
         <el-main>
             <el-card>
-                <div slot="header">产品列表</div>
+                <div slot="header">
+                    <span>产品列表</span>
+                    <el-button type="text" style="float: right; padding: 3px 0" @click="dialogFormVisible = true">新增产品</el-button>
+                    <el-dialog title="新增产品" :visible.sync="dialogFormVisible"
+                        :close-on-click-modal="false" :close-on-press-escape="false">
+                        <el-form :model="form" label-width="110px">
+                            <el-form-item label="产品名称">
+                                <el-input v-model="form.fldName" placeholder="请输入产品名称" auto-complete="off" />
+                            </el-form-item>
+                            <el-form-item label="产品系列">
+                                <el-select filterable v-model="form.fldSeries" placeholder="请选择产品系列">
+                                    <el-option v-for="i in seriesList" :key="i.fldValue" :label="i.fldName" :value="i.fldValue" />
+                                </el-select>
+                            </el-form-item>
+                            <el-form-item label="产品规格">
+                                <el-input v-model="form.fldSpec" placeholder="请输入产品规格" auto-complete="off" />
+                            </el-form-item>
+                            <el-form-item label="零售单价">
+                                <el-input-number v-model="form.fldPrice" placeholder="请输入零售单价" auto-complete="off" />
+                            </el-form-item>
+                            <el-form-item label="会员单价">
+                                <el-input-number v-model="form.fldVipPrice" placeholder="请输入会员单价" auto-complete="off" />
+                            </el-form-item>
+                            <el-form-item label="购物券">
+                                <el-input-number v-model="form.fldVipVoucher" placeholder="请输入购物券" auto-complete="off" />
+                            </el-form-item>
+                            <el-form-item>
+                                <el-button type="primary" @click="onSubmit">确定</el-button>
+                                <el-button @click="dialogFormVisible = false">取消</el-button>
+                            </el-form-item>
+                        </el-form>
+                    </el-dialog>
+                </div>
                 <el-row type="flex" justify="end">
                     <el-pagination
                         layout="total, sizes, prev, pager, next, jumper"
@@ -42,40 +74,61 @@
 import $ from 'jquery';
 
 export default {
-  name: 'Test',
-  data () {
-    return {
-      list: [],
-      pageData: [],
-      currentPage: 1,
-      pageSize: 10
-    }
-  },
-  methods: {
-      loadProductList() {
+    name: 'Test',
+    data () {
+        return {
+            seriesList: [{fldName: "aaa", fldValue: "1"}, {fldName: "bbbb", fldValue: "2"}],
+            list: [],
+            pageData: [],
+            currentPage: 1,
+            pageSize: 10,
+            dialogFormVisible: false,
+            form: {}
+        }
+    },
+    methods: {
+        loadProductList() {
+            var self = this;
+            $.ajax({
+                type: "GET",
+                dataType: "json",
+                url: "http://localhost/gl/product/list",
+                data: {},
+                success: function(data) {
+                    self.list = data.data;
+                    self.pageData = data.data;
+                }
+            });
+        },
+        onCurrentChange(page) {
+            this.currentPage = page;
+        },
+        onSizeChange(size) {
+            this.pageSize = size;
+        },
+        onSubmit() {
+            var self = this;
+            $.ajax({
+                type: "POST",
+                dataType: "json",
+                url: "http://localhost/gl/product/add",
+                data: self.form,
+                success: function(data) {
+                    if (data.result == "suc") {
+                        self.dialogFormVisible = false;
+                        self.loadProductList();
+                    }
+                    else {
+                        alert(data.msg);
+                    }
+                }
+            });
+        }
+    },
+    created() {
         var self = this;
-        $.ajax({
-            type: "GET",
-            dataType: "json",
-            url: "http://localhost/gl/product/list",
-            data: {},
-            success: function(data) {
-                self.list = data.data;
-                self.pageData = data.data;
-            }
-        });
-      },
-      onCurrentChange(page) {
-          this.currentPage = page;
-      },
-      onSizeChange(size) {
-          this.pageSize = size;
-      }
-  },
-  created() {
-    var self = this;
-    self.loadProductList();
-  }
+        self.loadProductList();
+    }
 }
 </script>
 
